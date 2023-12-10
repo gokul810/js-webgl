660e7a3027a2f044a5c48a229f62730b184ccf79
gokul810 <gk6790655@gmail.com>
2023-12-10 10:38:43 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -8,9 +8,12 @@
 */
 
 const canvas = document.getElementById("window");
-const gl = canvas.getContext("webgl"); if (!gl) {console.log("error: WebGL failed to load");}
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const gl = canvas.getContext("webgl"); if (!gl) {console.log("error: WebGL failed to load");}
+const info_text = document.getElementById("info-text");
+/* temp */
+if (canvas){info_text.innerHTML = `Scene Loaded canvas-w: ${window.innerwidth} canvas-h: ${window.innerheight}`}
 
 function draw() {
    gl.clearColor(0.0, 0.0, 0.0, 1.0);
